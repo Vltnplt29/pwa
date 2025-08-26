9becd4ca77bc35e03354eaf0a006f15547e2885b
Valentin Phil <Vltnplt@gmail.com>
2025-08-26 11:31:04 +0200

    Modif Css

--- install.js
+++ install.js
@@ -1,30 +1,35 @@
-const installBtn = document.querySelector('.install-btn');
-let deferredPrompt = null;
+const install = () => {
 
-// On cache le bouton par défaut
-installBtn.classList.add('hidden');
+    const installBtn = document.querySelector('.install-btn');
+    let deferredPrompt = null;
 
-// Événement quand l'app est "installable"
-window.addEventListener('beforeinstallprompt', e => {
-    e.preventDefault();
-    deferredPrompt = e;
-    installBtn.classList.remove('hidden');
-});
-
-// Clic sur le bouton d'installation
-installBtn.addEventListener('click', async e => {
-    e.preventDefault();
-    deferredPrompt.prompt();
-
-    const { outcome } = await deferredPrompt.userChoice;
-
-    if (outcome === 'accepted') {
-        installBtn.classList.add('hidden');
-    }
-    deferredPrompt = null;
-});
-
-// Quand l'app est installée
-window.addEventListener('appinstalled', () => {
+    // On cache le bouton par défaut
     installBtn.classList.add('hidden');
-});
+
+    // Événement quand l'app est "installable"
+    window.addEventListener('beforeinstallprompt', e => {
+        e.preventDefault();
+        deferredPrompt = e;
+        installBtn.classList.remove('hidden');
+    });
+
+    // Clic sur le bouton d'installation
+    installBtn.addEventListener('click', async e => {
+        e.preventDefault();
+        deferredPrompt.prompt();
+
+        const { outcome } = await deferredPrompt.userChoice;
+
+        if (outcome === 'accepted') {
+            installBtn.classList.add('hidden');
+        }
+        deferredPrompt = null;
+    });
+
+    // Quand l'app est installée
+    window.addEventListener('appinstalled', () => {
+        installBtn.classList.add('hidden');
+    });
+}
+
+export default install 
