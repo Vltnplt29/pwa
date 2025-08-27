47dac78a8531a6eb431b4e95911a3005c2fbb5ec
Valentin Phil <Vltnplt@gmail.com>
2025-08-27 08:40:07 +0200

    push notif SW

--- notifications.js
+++ notifications.js
@@ -1,34 +1,36 @@
 const notifications = () => {
-    const notifBtn = document.querySelector('.notif-me')
-    notifBtn.addEventListener('click', e => {
-        e.preventDefault()
-        let myNotification = null
+  const notifBtn = document.querySelector('.notif-me');
+  notifBtn.addEventListener('click', e => {
+    e.preventDefault();
+    let myNotification = null;
 
-        const options = {
-            body: "Texte de la notif",
-            icon: "/icons/favicon-16x16.png",
-            vibrate: [200,100,200,100,200,100,200]
-        }
-        if(!("Notification" in window)) {
-            alert('Pas de notifs dispos dans ce navigateur')
-        } else if (Notification.permission === 'granted') {
-            console.log('Notif accepted')
-            myNotification = new Notification("Hello", options)
-        } else {
-            Notification.requestPermission()
-            .then(permission => {
-                if(permission === 'granted') {
-                    console.log('Notif accepted')
-                    myNotification = new Notification("Hello", options)
-                    
-                }
-            })
-        }
-        myNotification.onclick = () => {
-            window.open("https://cepegra.be/", '_blank')
-        }
-        myNotification = null
-    })//onclick sur btn
+    const options = {
+      body: "Texte de la notif",
+      icon: "/icons/favicon-16x16.png",
+      vibrate: [200, 100, 200]
+    };
+
+    if (!("Notification" in window)) {
+      alert('Pas de notifs dispo dans ce navigateur');
+    } else if (Notification.permission === 'granted') {
+      console.log('Notif accepted');
+      myNotification = new Notification("Hello world", options);
+      myNotification.onclick = () => {
+        window.open('https://cepegra.be', '_blank');
+      };
+    } else {
+      Notification.requestPermission()
+        .then(permission => {
+          if (permission === 'granted') {
+            console.log('Notif accepted');
+            myNotification = new Notification("Hello world", options);
+            myNotification.onclick = () => {
+              window.open('https://cepegra.be', '_blank');
+            };
+          }
+        });
+    }
+  });
 }
 
-notifications()
+notifications();
--- sw.js
+++ sw.js
@@ -91,3 +91,26 @@ self.addEventListener("fetch", (e) => {
     })
   );
 });
+
+// push notifications
+
+self.addEventListener('push', e => {
+  if ( !(self.Notification && self.Notification.permission === 'granted'))
+  {
+    return
+  }
+  console.log('push')
+
+  const data = e.data?.json() ?? ()
+  const title = data.title || "Hello WF-12"
+  const url = data.url || "https://cepegra.be"
+  const message = data.message || "Message de la formation WF-12"
+  const icon = "/icons/favionc-256x256.png"
+
+  const notification = registration.showNotification ( title, {
+  
+    body : message,
+    icon
+  }
+  )
+})
